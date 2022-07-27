6ccf051139e5376db1812773a31f28c7e0a5340e
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-07-26 21:40:15 -0300

    fix

--- src/routes/[city].js
+++ src/routes/[city].js
@@ -3,7 +3,7 @@ export async function GET({ params }) {
   const url = `https://api.openweathermap.org/data/2.5/weather?appid=697cb7795cff3ff2640e6a4556a4f91a&units=metric&q=${params.city}`
   const resp = await fetch(url);
   const json = await resp.json();
-    const { weather, main, name } = json;
+    const { weather = [{}], main = {}, name } = json;
     const data = {
       main: weather[0].main,
       description: weather[0].description,
